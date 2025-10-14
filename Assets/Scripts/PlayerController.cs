4d4969df2142d25c0368aa22edae78e33d0db1c1
baxjuand <baxjuand@gmail.com>
2025-10-13 21:13:25 -0500

    Changed logic on how to apply facing direction to the player when moving horizontally

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] Animator animator;
 
+    private Vector3 originalScale;
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Collision: " + other.gameObject.name);
     }
 
+    void Start()
+    {
+        originalScale = transform.localScale;  
+    }
+
     void Update()
     {
-        Vector3 scale = transform.localScale;
-
         speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
-        if (speed < 0) { scale.x = -1f * Mathf.Abs(scale.x); }
-        else if (speed > 0) { scale.x = Mathf.Abs(scale.x); }
+
+        if (speed < 0) 
+        {
+            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z); 
+        }
+        else if (speed > 0) 
+        {
+            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z); 
+        }
             
     }
 }
